b10f2aebfdf732a59307867a2570bd453f027d34
kkawaguchi3 <kkawaguchi3@gatech.edu>
2017-09-15 13:14:44 -0400

    created a report tab

--- platforms/android/assets/www/js/my-app.js
+++ platforms/android/assets/www/js/my-app.js
@@ -5,13 +5,13 @@ var myApp = new Framework7();
 var $$ = Dom7;
 
 // Add views
-var view1 = myApp.addView('#view-1');
-var view2 = myApp.addView('#view-2', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
-var view3 = myApp.addView('#view-3');
-var view4 = myApp.addView('#view-4');
+ var view1 = myApp.addView('#view-1');
+// var view2 = myApp.addView('#view-2', {
+//     // Because we use fixed-through navbar we can enable dynamic navbar
+//     dynamicNavbar: true
+// });
+// var view3 = myApp.addView('#view-3');
+// var view4 = myApp.addView('#view-4');
 var phoneView = myApp.addView('#view-5');
 
 // Functions for Call plugin
