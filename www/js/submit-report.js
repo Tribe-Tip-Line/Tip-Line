5a7f8966b91e2e81d6a1eaed8fc0d5313d80cce6
Micah Jo <micahman97@gmail.com>
2017-09-22 12:43:11 -0400

    added functionality to take a picture, video, and get images and videos from the gallery; also added functionality to record audio, but cannot get from audio gallery yet

--- www/js/submit-report.js
+++ www/js/submit-report.js
@@ -13,15 +13,70 @@ $$('.confirm-title-ok-cancel').on('click', function () {
     );
 });
 
+// two different implementations - getting from library is done with plugin-camera (do phonegap plugin add cordova-plugin-camera)
+// getting from picture or video is from plugin-media-capture (do phonegap plugin add cordova-plugin-media-capture)
+
+// MEDIA CAPTURE
+
+// capture callback
+var captureSuccess = function(mediaFiles) {
+    var i, path, len;
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        path = mediaFiles[i].fullPath;
+        // do something interesting with the file
+    }
+};
+
+// capture error callback
+var captureError = function(error) {
+    return null;
+};
+
+
+// CAMERA
+
+
+function setOptions(srcType, isPicture) {
+    var mType;
+    if (isPicture) {
+        mType = Camera.MediaType.PICTURE;
+    } else {
+        mType = Camera.MediaType.VIDEO;
+    }
+    var option = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: mType,
+        allowEdit: true,
+        correctOrientation: true  //Corrects Android orientation quirks
+    };
+    return option;
+}
+
+function cameraError(error) {
+    console.debug("Couldn't get media: " + error, "app");
+}
 
 // function that generates action sheet when add image is pressed
 $$('.image-1').on('click', function () {
+    var options;
     var buttons = [
         {
             text: 'Take Picture',
+            onClick: function () {
+                navigator.device.capture.captureImage(captureSuccess, captureError, {limit:2});
+            }
         },
         {
-            text: 'Choose from Library'
+            text: 'Choose from Library',
+            onClick: function () {
+                options = setOptions(Camera.PictureSourceType.PHOTOLIBRARY, 1);
+                navigator.camera.getPicture(onSuccess, cameraError, options);
+            }
         },
         {
             text: 'Cancel',
@@ -31,14 +86,27 @@ $$('.image-1').on('click', function () {
     myApp.actions(buttons);
 });
 
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage'); // this element does not exist yet
+    image.src = imageURI;
+}
+
 // function that generates action sheet when add video is pressed
 $$('.video-1').on('click', function () {
+    var options;
     var buttons = [
         {
             text: 'Take Video',
+            onClick: function () {
+                navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:2});
+            }
         },
         {
-            text: 'Choose from Library'
+            text: 'Choose from Library',
+            onClick: function () {
+                options = setOptions(Camera.PictureSourceType.PHOTOLIBRARY, 0);
+                navigator.camera.getPicture(onSuccess, cameraError, options);
+            }
         },
         {
             text: 'Cancel',
@@ -53,6 +121,9 @@ $$('.audio-1').on('click', function () {
     var buttons = [
         {
             text: 'Take Audio',
+            onClick: function () {
+                navigator.device.capture.captureAudio(captureSuccess, captureError, {limit:2});
+            }
         },
         {
             text: 'Choose from Library'
