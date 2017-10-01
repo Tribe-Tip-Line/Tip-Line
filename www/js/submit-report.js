300e2c96bcf496269a6ce86ab9591093ab6c2302
kkawaguchi3 <kkawaguchi3@gatech.edu>
2017-09-30 22:35:02 -0400

    completed geolocation, updated report screen, and generate a report object

--- www/js/submit-report.js
+++ www/js/submit-report.js
@@ -1,9 +1,32 @@
 
+var report = {
+    "User": "test user"
+};
+
+var images = [];
+var videos = [];
+var audios = [];
+
+var date = new Date();
+
+
 // function that handles the submission of a report (when submit is pressed)
 $$('.confirm-title-ok-cancel').on('click', function () {
     myApp.confirm('Are you sure?', 'Submit a Report',
       function () {
-        //TODO: Add logic that submits a report to the database/email
+        //Constructs the report object with location, date, description, and its respective media
+        navigator.geolocation.getCurrentPosition(onSuccessfulGeolocationReport, onErrorGeolocation, {maximumAge: 300000, timeout: 30000, enableHighAccuracy : true });
+        report["Date"] = date.getDate();
+        report["Description"] = document.getElementById("report-description").value;
+        if (images.length > 0) {
+            report["Images"] = images;
+        }
+        if (videos.length > 0) {
+            report["Videos"] = videos;
+        }
+        if (audios.length > 0) {
+            report["Audios"] = audios;
+        }
         myApp.alert('Your report has successfully been submitted');
       },
       function () {
@@ -13,18 +36,69 @@ $$('.confirm-title-ok-cancel').on('click', function () {
     );
 });
 
+
+// Gets location of user to add in report object
+function onSuccessfulGeolocationReport(position) {
+    report["Location"] = position.coords.latitude + "," + position.coords.longitude;
+}
+
 // two different implementations - getting from library is done with plugin-camera (do phonegap plugin add cordova-plugin-camera)
 // getting from picture or video is from plugin-media-capture (do phonegap plugin add cordova-plugin-media-capture)
 
 // MEDIA CAPTURE
 
-// capture callback
-var captureSuccess = function(mediaFiles) {
+// capture callback for Images
+var captureSuccessImage = function(mediaFiles) {
     var i, path, len;
+    var element = document.getElementById("add-image");
     for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-        path = mediaFiles[i].fullPath;
+        path = mediaFiles[i].name;
+        images.push(mediaFiles[i]);
         // do something interesting with the file
     }
+    var text = "";
+    for (var j = 0; j < images.length; j++) {
+        text += "<img src=" + images[j].fullPath + ">" + "</img>&nbsp;";
+    }
+    if (element.innerHTML === "Add Image") {
+        element.innerHTML = text;
+    } else {
+        element.innerHTML += text;
+    }
+};
+
+// capture callback for Videos
+var captureSuccessVideo = function(mediaFiles) {
+    var i, path, len;
+    var element = document.getElementById("add-video");
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        path = mediaFiles[i].name;
+        videos.push(mediaFiles[i]);
+        // do something interesting with the file
+    }
+    var text = "";
+    for (var j = 0; j < videos.length; j++) {
+        text += videos[j].name + "<br>";
+    }
+    element.innerHTML = text;
+
+};
+
+// capture callback for Audio
+var captureSuccessAudio = function(mediaFiles) {
+    var i, path, len;
+    var element = document.getElementById("add-audio");
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        path = mediaFiles[i].name;
+        audios.push(mediaFiles[i]);
+        // do something interesting with the file
+    }
+    var text = "";
+    for (var j = 0; j < audios.length; j++) {
+        text += audios[j].name + "<br>";
+    }
+    element.innerHTML = text;
+
 };
 
 // capture error callback
@@ -68,14 +142,14 @@ $$('.image-1').on('click', function () {
         {
             text: 'Take Picture',
             onClick: function () {
-                navigator.device.capture.captureImage(captureSuccess, captureError, {limit:2});
+                navigator.device.capture.captureImage(captureSuccessImage, captureError, {limit:2});
             }
         },
         {
             text: 'Choose from Library',
             onClick: function () {
                 options = setOptions(Camera.PictureSourceType.PHOTOLIBRARY, 1);
-                navigator.camera.getPicture(onSuccess, cameraError, options);
+                navigator.camera.getPicture(onSuccessImage, cameraError, options);
             }
         },
         {
@@ -86,9 +160,22 @@ $$('.image-1').on('click', function () {
     myApp.actions(buttons);
 });
 
+// function callback for when picture is chosen from library
+function onSuccessImage(imageURI) {
+    var image = document.getElementById('add-image'); // this element does not exist yet
+    images.push(imageURI);
+    if (image.innerHTML == "Add Image") {
+        image.innerHTML = "<img src=" + imageURI + "></img>&nbsp;";
+    } else {
+        image.innerHTML += "<img src=" + imageURI + "></img>&nbsp;";
+    }
+
+}
+
 function onSuccess(imageURI) {
-    var image = document.getElementById('myImage'); // this element does not exist yet
-    image.src = imageURI;
+    var video = document.getElementById('add-video');
+    video.innerHTML = imageURI;
+
 }
 
 // function that generates action sheet when add video is pressed
@@ -98,16 +185,16 @@ $$('.video-1').on('click', function () {
         {
             text: 'Take Video',
             onClick: function () {
-                navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:2});
+                navigator.device.capture.captureVideo(captureSuccessVideo, captureError, {limit:2});
             }
         },
-        {
-            text: 'Choose from Library',
-            onClick: function () {
-                options = setOptions(Camera.PictureSourceType.PHOTOLIBRARY, 0);
-                navigator.camera.getPicture(onSuccess, cameraError, options);
-            }
-        },
+        // {
+        //     text: 'Choose from Library',
+        //     onClick: function () {
+        //         options = setOptions(Camera.PictureSourceType.PHOTOLIBRARY, 0);
+        //         navigator.camera.getPicture(onSuccess, cameraError, options);
+        //     }
+        // },
         {
             text: 'Cancel',
             color: 'red'
@@ -122,11 +209,8 @@ $$('.audio-1').on('click', function () {
         {
             text: 'Take Audio',
             onClick: function () {
-                navigator.device.capture.captureAudio(captureSuccess, captureError, {limit:2});
+                navigator.device.capture.captureAudio(captureSuccessAudio, captureError, {limit:2});
             }
-        },
-        {
-            text: 'Choose from Library'
         },
         {
             text: 'Cancel',
