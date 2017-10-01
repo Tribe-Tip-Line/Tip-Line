1bdf64b597ec38d027e76c23c6235c40b89c4d73
William <Wilche3@gmail.com>
2017-10-01 16:43:07 -0400

    Heather added login functionality here

--- platforms/android/assets/www/js/my-app.js
+++ platforms/android/assets/www/js/my-app.js
@@ -6,12 +6,12 @@ var $$ = Dom7;
 
 // Add views
 var view1 = myApp.addView('#view-1');
-var view2 = myApp.addView('#view-2', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
-var view3 = myApp.addView('#view-3');
-var view4 = myApp.addView('#view-4');
+// var view2 = myApp.addView('#view-2', {
+//     // Because we use fixed-through navbar we can enable dynamic navbar
+//     dynamicNavbar: true
+// });
+// var view3 = myApp.addView('#view-3');
+// var view4 = myApp.addView('#view-4');
 var phoneView = myApp.addView('#view-5');
 
 // Functions for Call plugin
@@ -34,17 +34,7 @@ function onSuccessfulGeolocation(position) {
     var element = document.getElementById('geolocation-test');
     var latlng = {lat: parseFloat(position.coords.latitude), lng: parseFloat(position.coords.longitude)};
     element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
-                           'Longitude: ' + position.coords.longitude     + '<br />'   
-    // var geocoder = new google.maps.Geocoder;
-    // geocoder.geocode({'location': latlng}, function(results, status) {
-    //     if (status === 'OK') {
-    //         if (results[0]) {
-    //             element.innerHTML = results[0].formatted_address;
-    //             //Below line is used to get strictly the country name from the address.
-    //             //element.innerHTML = 'Country: ' + results[0].address_components[6].long_name;
-    //         }
-    //     }
-    // })
+                           'Longitude: ' + position.coords.longitude     + '<br />'
 }
 
 // onError Callback receives a PositionError object
@@ -56,3 +46,29 @@ function onErrorGeolocation(error) {
 function geolocatePressed() {
     var watchID = navigator.geolocation.watchPosition(onSuccessfulGeolocation, onErrorGeolocation, {maximumAge: 300000, timeout: 30000, enableHighAccuracy : true });
 }
+
+function validateUser() {
+    var key = document.getElementById('reg-key').value;
+    if (key === 'XYZ') {
+        window.localStorage.setItem("key", key);
+        window.location.replace("registration.html");
+    } else {
+        //some warning for invalid input popup
+    }
+}
+
+function registerUser() {
+    //need to save to database once that is set up
+    window.location.replace("index.html");
+}
+
+function startScreen() {
+    //will need to change the if to check against keys in database
+    if (!(window.localStorage.getItem("key") === 'WXYZ')) {
+        window.location.replace("login.html");
+    }
+}
+
+
+
+
